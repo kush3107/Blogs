bea4fc27ae9b92caa03ca11950b11bf5b16b3d0c
Kushagra <kkss420@gmail.com>
2015-04-08 21:38:01 +0530

    all done almost

--- edit.php
+++ edit.php
@@ -1,19 +1,27 @@
-
+<?php
+require_once "config.php";
+$query = "SELECT * FROM `blogs` where `id` =" .$_GET['id'];
+$result=$connection->query($query);
+$listOfBlogs=$result->fetch_all(MYSQLI_ASSOC);
+$kush=$listOfBlogs[0];
+$query1="update `blogs` set `title`='{$_POST['title']}' `content`='{$_POST['content']}'"
+$result1=$connection->query($query1);
+?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
     <meta charset="UTF-8">
     <title>CodeKamp | Edit blog</title>
 </head>
-<body>q
+<body>
 <h2>Edit the fields below to edit a blog</h2>
-<form action="store.php" method="post">
-    <label for="title">Enter the title for your blog:</label>
-    <input id="title" type="text" name="title" value="" />
+<form method="post">
+    <label for="title">Edit the title for your blog:</label>
+    <input id="title" type="text" value="<?php echo $kush['title']; ?>" />
 
     <br></br>
-    <label for="content">Enter the content of the blog:</label>
-    <textarea id="content" type="text" name="content" placeholder="enter the content" ></textarea>
+    <label for="content">Edit the contents of your blog:</label>
+    <textarea id="content" type="text" name="content" placeholder="enter the content" ><?= $kush['content'] ?></textarea>
     <br></br>
     <input type="submit" value="submit" >
 </form>
--- index.php
+++ index.php
@@ -3,28 +3,48 @@ require_once "config.php";
 $query="SELECT*FROM `blogs`";
 $result=$connection->query($query);
 $listOfBlogs=$result->fetch_all(MYSQLI_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title>CodeKamp | list of all blogs</title>
+    <title>CodeKamp | Blogs</title>
 </head>
+<div class="div"></div>
 <body>
-<h1 style="text-align: center">All the blogs are listed below:</h1>
-
-
+<h1>All the blogs are listed below:</h1>
 <ul>
 <?php
-
-
 foreach($listOfBlogs as $blog) {
     echo '<li><a href="view.php?id='.$blog['id'].' ">'.$blog['title'].'</a></li>';
 }
-
 ?>
 </ul>
+</div>
+<div class="div">
+    <h2><a href="create.php"> To create a blog click on this link</a></h2>
+</div>
+<div class="div">
+    <h2>To edit any blog click on it</h2>
+    <ul>
+        <?php
+        foreach($listOfBlogs as $blog) {
+            echo '<li><a href="edit.php?id='.$blog['id'].' ">'.$blog['title'].'</a></li>';
+        }
+        ?>
+    </ul>
+
+</div>
+<div class="div" id="link">
+<h2>To delete any blog click on it</h2>
+    <ul>
+        <?php
+        foreach($listOfBlogs as $blog) {
+            echo '<li><a href="view.php?id='.$blog['id'].' ">'.$blog['title'].'</a></li>';
+        }
+        ?>
+    </ul>
+</div>
 </body>
 </html>
